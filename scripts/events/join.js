79d17cbb6c27ea0f2659f0c9b04c7fd0bb28047d
Nirob-oe <148666328+Nirob-oe@users.noreply.github.com>
2025-02-02 23:44:44 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -48,7 +48,7 @@ module.exports.run = async function({ api, event, Users }) {
   if (event.logMessageData.addedParticipants && Array.isArray(event.logMessageData.addedParticipants) && event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
     //api.changeNickname(`𝗕𝗢𝗧 ${(!global.config.BOTNAME) ? "Buddy" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
 
-    let gifUrl = 'https://i.postimg.cc/cLB1wdcG/https-c-tenor-com-Ub-QBW6a-VISQAAAAC-spy-x-family-anya.gif';
+    let gifUrl = 'https://imgur.com/a/xrkaoS5';
 let gifPath = __dirname + '/Nayan/join/join.gif';
 
 axios.get(gifUrl, { responseType: 'arraybuffer' })
