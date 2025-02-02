69bdbb044d544cef7bd43f2a61292aa599e6cb9f
Nirob-oe <148666328+Nirob-oe@users.noreply.github.com>
2025-02-02 22:46:34 +0600

    Update and rename Anime.js to Anime2.js

--- scripts/commands/Anime2.js
+++ scripts/commands/Anime2.js
@@ -1,7 +1,7 @@
 
 /** I am doing this coding with a lot of difficulty, please don't post it yourself¯\_(ツ)_/¯ **/
 module.exports.config = {
-  name: "kakashiv2", 
+  name: "kakashi1", 
   version: "1.0.0", 
   permission: 0,
   credits: "Nayan",
