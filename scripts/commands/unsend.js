15a539d68fe9490220b4721d6f8eafae6388581a
Nirob-oe <148666328+Nirob-oe@users.noreply.github.com>
2025-02-02 15:13:26 +0600

    Update unsend.js

--- scripts/commands/unsend.js
+++ scripts/commands/unsend.js
@@ -1,33 +1,29 @@
-module.exports = {
-	config: {
-		name: "uns",
-		version: "1.2",
-		author: "NTKhang",
-		countDown: 5,
-		role: 0,
-		description: {
-			vi: "Gỡ tin nhắn của bot",
-			en: "Unsend bot's message"
-		},
-		category: "box chat",
-		guide: {
-			vi: "reply tin nhắn muốn gỡ của bot và gọi lệnh {pn}",
-			en: "reply the message you want to unsend and call the command {pn}"
-		}
-	},
-
-	langs: {
-		vi: {
-			syntaxError: "Vui lòng reply tin nhắn muốn gỡ của bot"
-		},
-		en: {
-			syntaxError: "Please reply the message you want to unsend"
-		}
-	},
-
-	onStart: async function ({ message, event, api, getLang }) {
-		if (!event.messageReply || event.messageReply.senderID != api.getCurrentUserID())
-			return message.reply(getLang("syntaxError"));
-		message.unsend(event.messageReply.messageID);
-	}
+module.exports.config = {
+	name: "uns",
+	version: "1.0.5",
+	permission: 2,
+	credits: "Nayan",
+	prefix: true,
+	description: "ban or unblock users",
+	category: "admin",
+	usages: " ",
+	cooldowns: 5
 };
+
+
+module.exports.languages = {
+	"vi": {
+		"returnCant": "Không thể gỡ tin nhắn của người khác.",
+		"missingReply": "Hãy reply tin nhắn cần gỡ."
+	},
+	"en": {
+		"returnCant": "Can't to unsend message from other user.",
+		"missingReply": "Reply to the message you want to unsend."
+	}
+}
+
+module.exports.run = function({ api, event, getText }) {
+	if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
+	if (event.type != "message_reply") return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
+	return api.unsendMessage(event.messageReply.messageID);
+}
