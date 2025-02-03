dd7452170fdc757b251019713c85a862bf010ef6
Nirob-oe <148666328+Nirob-oe@users.noreply.github.com>
2025-02-03 13:31:54 +0600

    Update Admin2.js

--- scripts/commands/Admin2.js
+++ scripts/commands/Admin2.js
@@ -29,8 +29,8 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
 𝐍𝐚𝐦𝐞       : ARIYAN
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : sung jin woo
-𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : Ariyan Ahmed
+𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : Islam
 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: RAJSHAHI
 𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: RAJSHAHI
 𝐆𝐞𝐧𝐝𝐞𝐫.   : Male
